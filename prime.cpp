59d5eb8cbda04d91c830aeb1ea6b76652ebd42ad
m1n199honey <m1n199ada@gmail.com>
2022-09-09 09:52:51 +0000

    update...

--- prime.cpp
+++ prime.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 int prime(int n){
-    // int f=1;
+    // int 9f=1;
     if(n==1) return 0;
-    if(n==2||n==3) return 1;
+    if(n==2) return 1;
     if(n%2==0) return 0;
-    for(int i=5;i<n;i+=2){
+    for(int i=3;i<n;i+=2){
         if(n%i==0) return 0;
     }
     return 1;
